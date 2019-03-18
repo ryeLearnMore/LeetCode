3dc4f1cadd72ced105ae447970560fc19c5be03e
rye <30286817+ryeLearnMore@users.noreply.github.com>
2019-03-18 22:04:48 +0800

    Add files via upload

---
+++ 062_unique-paths.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#@author: rye
+#@time: 2019/3/18
+
+# 当初的想法和这个类似：https://blog.csdn.net/AosChen/article/details/79810348
+# 就是用递归，结果又没跳出去那个固定的坑
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        def check(i, y, cnt):
+            if i == m - 1 and y == n - 1:
+                cnt += 1
+                return cnt
+            elif i < m - 1:
+                # i += 1
+                return check(i + 1, y, cnt)
+            else:
+                # y += 1
+                return check(i, y + 1, cnt)
+        return check(0, 0, 0)
+
+'''
+感想：
+动态规划就是强，不能总是想着用递归解决这些问题了，因为递归复杂度太高，并不适合解决所有问题。
+而且递归在return时跳不出去的坑还是没有理解。
+
+方法：
+    用一维数组代替了二维数据，涉及真的很巧妙，需要学习。
+'''
+class Solution1:
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        if m < 1 or n < 1:
+            return 0
+        dp = [0] *n
+        dp[0] = 1
+        for i in range(0,m):
+            for j in range(1,n):
+                dp[j] += dp[j-1]
+        return dp[n-1]
+
+if __name__ == '__main__':
+    m = 7
+    n = 3
+    print(Solution1().uniquePaths(m, n))
